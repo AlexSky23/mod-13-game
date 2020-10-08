16f19f3ac1f1076d045392404dcdf0ddc718eb46
Alex Sky <alex.sky23@yandex.ru>
2020-10-08 21:23:20 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,57 @@
+window.onload = function () {
+    // tasks,
+    //     timerField,
+    //     timeSetting,
+    //     gameStatusField,
+    //     gamePlayers,
+    //     textFieldQuestion,
+    //     textFieldAnswer1,
+    //     textFieldAnswer2,
+    //     textFieldAnswer3,
+    //     textFieldAnswer4,
+    //     textFieldAnswer5
+
+    var time1 = parseInt(prompt("Сколько времени будем играть?", 60));
+    //console.log(time1);
+    timeSetting.value = (time1)? time1 : 60;
+//console.log(timeSetting.value);
+
+    tikTakBoom.init(
+        tasks,
+        // Количество игроков
+        document.querySelector('#gamePlayers').value,
+
+        // Таймер
+        document.querySelector('#timerField'),
+
+        // Время на игру
+        document.querySelector('#timeSetting').value,
+        // Статус
+        document.querySelector('#gameStatusField'),
+
+        // Вопрос
+        document.querySelector('#questionField'),
+        // Ответы
+        document.querySelector('#answer1'),
+        document.querySelector('#answer2'),
+        document.querySelector('#answer3'),
+        document.querySelector('#answer4'),
+        document.querySelector('#answer5')
+    )
+
+    var game = document.querySelector('.game');
+    var gameStart = document.querySelector('.game__start');
+    var gameEnd = document.querySelector('.game__end');
+
+    gameStart.addEventListener('click', function () {
+        game.classList.add('game--active');
+        console.log(document.querySelector('#timeSetting').value);
+        tikTakBoom.run();
+    });
+
+    gameEnd.addEventListener('click', function () {
+        game.classList.remove('game--active');
+    });
+
+
+}
---
+++ js/tikTakBoom.js
@@ -0,0 +1,187 @@
+tikTakBoom = {
+    init(
+        tasks,
+        gamePlayers,
+
+        timerField,
+        timeSetting,
+        gameStatusField,
+
+        textFieldQuestion,
+        textFieldAnswer1,
+        textFieldAnswer2,
+        textFieldAnswer3,
+        textFieldAnswer4,
+        textFieldAnswer5
+    ) {
+        this.boomTimer = timeSetting;
+        this.superGame = false;
+        this.countOfPlayers = 2;
+        this.tasks = JSON.parse(tasks);
+
+        this.timerField = timerField;
+        this.timeSetting = timeSetting;
+        this.gamePlayers = gamePlayers;
+        this.players = undefined;
+        this.currentPlayer = undefined;
+        this.playerTimer = undefined;
+        this.gameStatusField = gameStatusField;
+        this.textFieldQuestion = textFieldQuestion;
+        this.textFieldAnswer1 = textFieldAnswer1;
+        this.textFieldAnswer2 = textFieldAnswer2;
+        this.textFieldAnswer3 = textFieldAnswer3;
+        this.textFieldAnswer4 = textFieldAnswer4;
+        this.textFieldAnswer5 = textFieldAnswer5;
+
+        this.needRightAnswers = 19;
+    },
+
+    run() {
+        this.startTimer(3);
+        this.state = 1;
+
+        this.rightAnswers = 0;
+
+        this.turnOn();
+
+        this.timer();
+    },
+
+    turnOn() {
+        this.gameStatusField.innerText += ` Вопрос игроку №${this.state}`;
+
+        const taskNumber = randomIntNumber(this.tasks.length - 1);
+        this.printQuestion(this.tasks[taskNumber]);
+
+        this.tasks.splice(taskNumber, 1);
+
+        this.state = (this.state === this.countOfPlayers) ? 1 : this.state + 1;
+    },
+
+    turnOff(value) {
+        if (this.currentTask[value].result) {
+            this.gameStatusField.innerText = 'Верно!';
+            this.rightAnswers += 1;
+            this.boomTimer += 5;
+        } else {
+            this.gameStatusField.innerText = 'Неверно!';
+            this.boomTimer -= 5;
+        }
+        if (this.rightAnswers < this.needRightAnswers) {
+            if (this.tasks.length === 0) {
+                this.finish('lose');
+            } else {
+                this.turnOn();
+            }
+        } else {
+            this.finish('won');
+        }
+
+        this.textFieldAnswer1.removeEventListener('click', answer1);
+        this.textFieldAnswer2.removeEventListener('click', answer2);
+        this.textFieldAnswer3.removeEventListener('click', answer3);
+        this.textFieldAnswer4.removeEventListener('click', answer4);
+        this.textFieldAnswer5.removeEventListener('click', answer5);
+    },
+
+    startGame(number = 0) {
+        if (this.players === undefined) {
+            this.players = creatDataPlayers(this.gamePlayers, this.boomTimer);
+        }
+
+        if (this.superGame === false) {
+            this.boomTimer = parseInt(this.timeSetting);
+        }
+
+        if (this.superGame === true) {
+            for (player of this.players) {
+                player.timer = this.boomTimer;
+                player.errors = 0;
+                player.score = 0;
+            }
+        }
+
+        this.currentPlayer = number;
+        this.startTimer();
+    },
+
+    printQuestion(task) {
+        this.textFieldQuestion.innerText = task.question;
+        this.textFieldAnswer1.innerText = task.answer1.value;
+        this.textFieldAnswer2.innerText = task.answer2.value;
+        this.textFieldAnswer3.innerText = task.answer3.value;
+        this.textFieldAnswer4.innerText = task.answer4.value;
+        this.textFieldAnswer5.innerText = task.answer5.value;
+
+        this.textFieldAnswer1.addEventListener('click', answer1 = () => this.turnOff('answer1'));
+        this.textFieldAnswer2.addEventListener('click', answer2 = () => this.turnOff('answer2'));
+        this.textFieldAnswer3.addEventListener('click', answer3 = () => this.turnOff('answer3'));
+        this.textFieldAnswer4.addEventListener('click', answer4 = () => this.turnOff('answer4'));
+        this.textFieldAnswer5.addEventListener('click', answer5 = () => this.turnOff('answer5'));
+
+        this.currentTask = task;
+    },
+
+    finish(result = 'lose') {
+        this.state = 0;
+        if (result === 'lose') {
+            this.gameStatusField.innerText = `Вы проиграли!`;
+            this.timerField.innerText = "THE END";
+        }
+        if (result === 'won') {
+            this.gameStatusField.innerText = `Вы выиграли!`;
+        }
+
+        this.textFieldQuestion.innerText = ``;
+        this.textFieldAnswer1.innerText = ``;
+        this.textFieldAnswer2.innerText = ``;
+        this.textFieldAnswer3.innerText = ``;
+        this.textFieldAnswer4.innerText = ``;
+        this.textFieldAnswer5.innerText = ``;
+
+        console.log(this);
+    },
+
+    timer() {
+        if (this.state) {
+            this.boomTimer -= 1;
+            let sec = this.boomTimer % 60;
+            let min = (this.boomTimer - sec) / 60;
+            sec = (sec >= 10) ? sec : '0' + sec;
+            min = (min >= 10) ? min : '0' + min;
+            this.timerField.innerText = `${min}:${sec}`;
+
+            if (this.boomTimer > 0) {
+                setTimeout(
+                    () => {
+                        this.timer()
+                    },
+                    1000,
+                )
+            } else {
+                this.finish('lose');
+            }
+        }
+    },
+    startTimer(timeTraining) {
+        //var timeTraining = 3;
+        console.log(timeTraining);
+        if(timeTraining > 0) {
+            timerField.innerText = timeTraining;
+            timeTraining-=1;
+            setTimeout(startTimer, 1000, timeTraining);
+        }
+        else {timerField.innerText = `START`};
+        //this.timerField.innerText = `3`;
+        //this.gameStatusField.innerText = `Ход ${this.players[this.currentPlayer].name} через`;
+        //let a = this;
+        //const startTimer = 
+        //setInterval(function () {
+            
+        //    this.timerField.innerText = `${timeTraining}`;
+        //    if(timeTraining === 0) {};
+
+       // }, 1000);
+
+    }
+}
